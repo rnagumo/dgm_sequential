f4db7a907de535e9cd4605da5db33d376ac2467f
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-02-18 20:51:42 +0900

    [add] BaseSequentialModel class for object oriented programing of deep sequential model.

---
+++ sequence/model/base.py
@@ -0,0 +1,86 @@
+
+"""Base class for deep sequential model"""
+
+
+import tqdm
+
+import torch
+import pixyz.models as pxm
+
+
+class BaseSequentialModel(pxm.Model):
+    def __init__(self, device, t_dim, **kwargs):
+        super().__init__(**kwargs)
+
+        self.device = device
+        self.t_dim = t_dim
+
+    def run(self, loader, epoch, training=True):
+
+        # Returned values
+        total_loss = 0
+        total_len = 0
+        results = []
+
+        # Train with mini-batch
+        for x, seq_len in tqdm.tqdm(loader):
+            # Input dimension must be (timestep_size, batch_size, feature_size)
+            x = x.transpose(0, 1).to(self.device)
+            data = {"x": x}
+            minibatch_size = x.size(1)
+
+            # Mask for sequencial data
+            mask = torch.zeros(x.size(0), x.size(1)).to(self.device)
+            for i, v in enumerate(seq_len):
+                mask[:v, i] += 1
+
+            # Initialize latent variable
+            data.update(self._init_variable(minibatch_size, x=x))
+
+            # Train / test
+            if training:
+                _loss = super().train(data, mask=mask, epoch=epoch,
+                                      results=results)
+            else:
+                _loss = super().test(data, mask=mask, epoch=epoch)
+
+            # Add training results
+            total_loss += _loss * minibatch_size
+            total_len += seq_len.sum()
+
+        # Return value
+        loss_dict = {"loss": (total_loss / total_len).item()}
+
+        if results:
+            results = torch.tensor(results).sum(axis=0)
+            loss_dict["annealing_factor"] = results[0]
+            loss_dict["cross_entropy"] = results[1]
+            loss_dict["kl_divergence"] = results[2]
+
+        return loss_dict
+
+    def sample(self):
+
+        # Get initial values
+        data = self._init_variable(1)
+
+        x = []
+        with torch.no_grad():
+            for _ in range(self.t_dim):
+                # Sample
+                x_t, data = self._sample_one_step(data)
+
+                # Add to data list
+                x.append(x_t)
+
+            # Data of size (batch_size, seq_len, input_size)
+            x = torch.cat(x).transpose(0, 1)
+
+        # Return data of size (1, batch_size, seq_len, input_size)
+        return x[:, None]
+
+    def _init_variable(self, minibatch_size, **kwargs):
+        raise NotImplementedError
+
+    def _sample_one_step(self, data, **kwargs):
+        raise NotImplementedError
--- sequence/train.py
+++ sequence/train.py
@@ -4,82 +4,15 @@
 import argparse
 import pathlib
 
-import tqdm
-
 import torch
 from torch.utils import tensorboard
 
 from dataset.polydata import init_poly_dataloader
-from model.dmm import load_dmm_model, init_dmm_var, get_dmm_sample
+from model.dmm import DMM
 from model.srnn import load_srnn_model, init_srnn_var, get_srnn_sample
 from model.storn import load_storn_model, init_storn_var, get_storn_sample
 from model.vrnn import load_vrnn_model, init_vrnn_var, get_vrnn_sample
 from utils.utils import init_logger, load_config, check_logdir
-
-
-def data_loop(epoch, loader, model, args, config, train_mode=True):
-
-    device = config["device"]
-
-    # Returned values
-    total_loss = 0
-    total_len = 0
-    results = []
-
-    # Train with mini-batch
-    for x, seq_len in tqdm.tqdm(loader):
-        # Input dimension must be (timestep_size, batch_size, feature_size)
-        x = x.transpose(0, 1).to(device)
-        data = {"x": x}
-        minibatch_size = x.size(1)
-
-        # Mask for sequencial data
-        mask = torch.zeros(x.size(0), x.size(1)).to(device)
-        for i, v in enumerate(seq_len):
-            mask[:v, i] += 1
-
-        # Initialize latent variable
-        data.update(config["init_func"](minibatch_size, config, x=x))
-
-        # Train / test
-        if train_mode:
-            _loss = model.train(data, mask=mask, epoch=epoch, results=results)
-        else:
-            _loss = model.test(data, mask=mask, epoch=epoch)
-
-        # Add training results
-        total_loss += _loss * minibatch_size
-        total_len += seq_len.sum()
-
-    # Save training log to config
-    if results:
-        results = torch.tensor(results).sum(axis=0)
-        config["annealing_factor"] = results[0]
-        config["cross_entropy"] = results[1]
-        config["kl_divergence"] = results[2]
-
-    return total_loss / total_len
-
-
-def draw_image(sampler, args, config):
-
-    # Get update parameters
-    data = config["init_func"](1, config)
-
-    x = []
-    with torch.no_grad():
-        for _ in range(config["t_dim"]):
-            # Sample
-            x_t, data = config["sample_func"](sampler, data)
-
-            # Add to data list
-            x.append(x_t)
-
-        # Data of size (batch_size, seq_len, input_size)
-        x = torch.cat(x).transpose(0, 1)
-
-        # Return data of size (1, batch_size, seq_len, input_size)
-        return x[:, None]
 
 
 def train(args, logger, config):
@@ -121,7 +54,13 @@ def train(args, logger, config):
     # 3. Model
     # -------------------------------------------------------------------------
 
-    model, sampler = config["load_func"](config)
+    if args.model == "dmm":
+        model = DMM(x_dim=x_dim, t_dim=t_dim, device=device,
+                    **config["dmm_params"],
+                    anneal_params=config["anneal_params"],
+                    optimizer_params=config["optimizer_params"])
+    else:
+        raise KeyError
 
     # -------------------------------------------------------------------------
     # 4. Training
@@ -131,39 +70,35 @@ def train(args, logger, config):
         logger.info(f"--- Epoch {epoch} ---")
 
         # Training
-        train_loss = data_loop(epoch, train_loader, model, args, config, True)
-        valid_loss = data_loop(epoch, valid_loader, model, args, config, False)
-        test_loss = data_loop(epoch, test_loader, model, args, config, False)
+        train_loss = model.run(train_loader, epoch, training=True)
+        valid_loss = model.run(valid_loader, epoch, training=False)
+        test_loss = model.run(test_loader, epoch, training=False)
 
         # Sample data
-        sample = draw_image(sampler, args, config)
+        sample = model.sample()
 
         # Log
-        writer.add_scalar("Loss/train_loss", train_loss.item(), epoch)
-        writer.add_scalar("Loss/valid_loss", valid_loss.item(), epoch)
-        writer.add_scalar("Loss/test_loss", test_loss.item(), epoch)
+        writer.add_scalar("loss/train_loss", train_loss["loss"], epoch)
+        writer.add_scalar("loss/valid_loss", valid_loss["loss"], epoch)
+        writer.add_scalar("loss/test_loss", test_loss["loss"], epoch)
         writer.add_images("image_from_latent", sample, epoch)
-        writer.add_scalar("Training/annealing_factor",
-                          config["annealing_factor"], epoch)
-        writer.add_scalar("Training/cross_entropy",
-                          config["cross_entropy"], epoch)
-        writer.add_scalar("Training/kl_divergence",
-                          config["kl_divergence"], epoch)
+        writer.add_scalar("training/annealing_factor",
+                          train_loss["annealing_factor"], epoch)
+        writer.add_scalar("training/cross_entropy",
+                          train_loss["cross_entropy"], epoch)
+        writer.add_scalar("training/kl_divergence",
+                          train_loss["kl_divergence"], epoch)
 
-        logger.info(f"Train loss = {train_loss.item()}")
-        logger.info(f"Valid loss = {valid_loss.item()}")
-        logger.info(f"Test loss = {test_loss.item()}")
+        logger.info(f"Train loss = {train_loss['loss']}")
+        logger.info(f"Valid loss = {valid_loss['loss']}")
+        logger.info(f"Test loss = {test_loss['loss']}")
 
     # Log hyper-parameters
     hparam_dict = vars(args)
-    for key, d in config.items():
-        if "_params" in key:
-            hparam_dict.update(d)
-
-    metric_dict = {"train_loss": train_loss.item(),
-                   "valid_loss": valid_loss.item(),
-                   "test_loss": test_loss.item()}
-    writer.add_hparams(hparam_dict, metric_dict)
+    hparam_dict.update(config["anneal_params"])
+    hparam_dict.update(config["optimizer_params"])
+    hparam_dict.update(config[f"{args.model}_params"])
+    writer.add_hparams(hparam_dict, {})
 
     writer.close()
 
@@ -201,19 +136,6 @@ def main():
     # Config
     config = load_config(args.config)
     logger.info(f"Configs: {config}")
-
-    # Model
-    load_func = {"dmm": load_dmm_model, "vrnn": load_vrnn_model,
-                 "srnn": load_srnn_model, "storn": load_storn_model}
-    init_func = {"dmm": init_dmm_var, "vrnn": init_vrnn_var,
-                 "srnn": init_srnn_var, "storn": init_storn_var}
-    sample_func = {"dmm": get_dmm_sample, "vrnn": get_vrnn_sample,
-                   "srnn": get_srnn_sample, "storn": get_storn_sample}
-    config.update({
-        "load_func": load_func[args.model],
-        "init_func": init_func[args.model],
-        "sample_func": sample_func[args.model],
-    })
 
     try:
         train(args, logger, config)
