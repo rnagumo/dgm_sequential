ce04a2f22237c979b62d8a1cfe430b1ee8b6575a
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-02-25 22:01:52 +0900

    [update] iteration loss with 1. time step expectation, 2. slice_step.

--- src/model/iteration_loss.py
+++ src/model/iteration_loss.py
@@ -8,16 +8,19 @@ import pixyz.utils as pxu
 class MonitoredIterativeLoss(pxl.IterativeLoss):
     """Iterative loss with monitoring CE and KL"""
 
-    def __init__(self, cross_entropy, kl_divergence, beta, **kwargs):
-        super().__init__(cross_entropy + beta * kl_divergence, **kwargs)
+    def __init__(self, cross_entropy, kl_divergence, beta, p=None, **kwargs):
+
+        if p is None:
+            _step_loss = cross_entropy + beta * kl_divergence
+        else:
+            _step_loss = (cross_entropy + beta * kl_divergence).expectation(p)
+
+        super().__init__(_step_loss, **kwargs)
 
         self.cross_entropy = cross_entropy
         self.kl_divergence = kl_divergence
         self.beta = beta
-
-    @property
-    def _symbol(self):
-        raise NotImplementedError
+        self.p = p
 
     def _get_eval(self, x_dict, **kwargs):
         series_x_dict = pxu.get_dict_values(
@@ -42,34 +45,45 @@ class MonitoredIterativeLoss(pxl.IterativeLoss):
 
         for t in range(max_iter):
             if self.slice_step:
+                # Update time step variable, and sample data from slice_step
                 x_dict.update({self.timestep_var[0]: t})
+                x_dict = self.slice_step.sample(x_dict)
             else:
                 # update series inputs & use slice_step_fn
                 x_dict.update(self.slice_step_fn(t, series_x_dict))
 
-            # evaluate
+            # If p exists, sample variable from p
+            if self.p:
+                x_dict = self.p.sample(x_dict)
+
+            # Evaluate
             _ce_loss, samples_1 = self.cross_entropy.eval(
                 x_dict, return_dict=True)
             _kl_loss, samples_2 = self.kl_divergence.eval(
                 x_dict, return_dict=True)
 
+            # Update variable
             x_dict.update(samples_1)
             x_dict.update(samples_2)
 
+            # Set mask
             if mask is not None:
                 _ce_loss *= mask[t]
                 _kl_loss *= mask[t]
 
+            # Accumulate losses
             _ce_loss_sum += _ce_loss
             _kl_loss_sum += _kl_loss
 
-            # update
+            # Update
             for key, value in self.update_value.items():
                 x_dict.update({value: x_dict[key]})
 
+        # Calculate ELBO loss
         beta = self.beta.eval(x_dict)
         loss = _ce_loss_sum + beta * _kl_loss_sum
 
+        # Save losses to results list
         if "results" in kwargs:
             kwargs["results"].append([_ce_loss_sum.sum().item(),
                                       _kl_loss_sum.sum().item()])
